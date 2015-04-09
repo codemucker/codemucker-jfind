9487aa5ba15c1cb6a894fbee3e065a11586a8820
Bert van Brakel <bertvanbrakel@gmail.com>
2015-04-09 11:28:30 +0930

    expose method name in ReflectedMethod

--- src/main/java/org/codemucker/jfind/ReflectedMethod.java
+++ src/main/java/org/codemucker/jfind/ReflectedMethod.java
@@ -21,4 +21,8 @@ public class ReflectedMethod extends AbstractReflectedObject {
     public Method getUnderlying(){
         return method;
     }
+    
+    public String getName(){
+    	return method.getName();
+    }
 }
